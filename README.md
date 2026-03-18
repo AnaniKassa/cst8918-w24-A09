756bbff818d6b7197ae9468631cd117efd119baa
AnaniKassa <kass0112@algonquinlive.com>
2026-03-17 22:42:20 -0400

    tested husky

---
+++ README.md
@@ -0,0 +1,5 @@
+# About me
+### Full name: Anani Thierry Kassa
+### Student ID: 041140713
+
+## Steps to follow
---
+++ infrastructure/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.2.4"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.68" # or latest stable
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "a09-rg"
+  location = "Canada Central"
+}
