e70e2b7308752700b5372ef3eeccf005e4a86faf
AnaniKassa <kass0112@algonquinlive.com>
2026-03-18 00:07:17 -0400

    test GitHub Actions failure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,12 +4,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.68" # or latest stable
+    version = "~> 3.68" # or latest stable
     
   }
 }
 
+
 resource "azurerm_resource_group" "example" {
-  name     = "a09-rg"
-  location = "Canada Central"
+name = "a09-rg"
+location = "Canada Central"
 }
