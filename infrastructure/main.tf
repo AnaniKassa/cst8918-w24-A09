a495cbe777b5b0710c81dc8bb7c9725eb0aa2e29
AnaniKassa <kass0112@algonquinlive.com>
2026-03-18 22:15:54 -0400

    introducing broken main

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,12 +5,12 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.68" # or latest stable
-
+    }
   }
 }
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "a09-rg"
-  location = "Canada Central"
+name     = "a09-rg"
+location = "Canada Central"
 }
