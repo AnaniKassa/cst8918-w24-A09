be6e6e8e645f53a541c2d0761eff9d689813fb6e
AnaniKassa <kass0112@algonquinlive.com>
2026-03-18 23:20:02 -0400

    brought a broken main

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,14 +5,14 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
     version = "~> 3.68" # or latest stable
-    
+    }
   }
 }
 
 
 resource "azurerm_resource_group" "example" {
-    name = "a09-rg"
-    location = "Canada Central"
+name = "a09-rg"
+location = "Canada Central"
 }
 # add a comment
 # test fix
